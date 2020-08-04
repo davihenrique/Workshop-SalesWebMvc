8523cb6eabe92b6ec06be474c43aa26ccef6ab12
Davi Silva <davih306@outlook.com>
2020-08-03 21:30:16 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Migrations/20200804002429_DepartmentForeignKey.Designer.cs
+++ SalesWebMvc/Migrations/20200804002429_DepartmentForeignKey.Designer.cs
@@ -9,8 +9,8 @@ using SalesWebMvc.Models;
 namespace SalesWebMvc.Migrations
 {
     [DbContext(typeof(SalesWebMvcContext))]
-    [Migration("20200731221632_OtherEntities")]
-    partial class OtherEntities
+    [Migration("20200804002429_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -60,7 +60,7 @@ namespace SalesWebMvc.Migrations
 
                     b.Property<DateTime>("BirthDate");
 
-                    b.Property<int?>("DepartmentId");
+                    b.Property<int>("DepartmentId");
 
                     b.Property<string>("Email");
 
@@ -84,7 +84,8 @@ namespace SalesWebMvc.Migrations
                 {
                     b.HasOne("SalesWebMvc.Models.Department", "Department")
                         .WithMany("Sellers")
-                        .HasForeignKey("DepartmentId");
+                        .HasForeignKey("DepartmentId")
+                        .OnDelete(DeleteBehavior.Cascade);
                 });
 #pragma warning restore 612, 618
         }
